67ad4696703722fa498910953ba5f17112762749
Daan <daan@mostlyoriginal.net>
2014-06-22 12:42:26 +0200

    Version bump 0.2.2

--- output/lua/bandaid/aid_Client.lua
+++ output/lua/bandaid/aid_Client.lua
@@ -1,6 +1,6 @@
 decoda_name = "Client"
 Script.Load("lua/PreLoadMod.lua")
 
-Shared.Message('SCC\'s bandaid [ALPHA] V0.2.1')
+Shared.Message('SCC\'s bandaid [ALPHA] V0.2.2')
 
 Script.Load("lua/PostLoadMod.lua") 
